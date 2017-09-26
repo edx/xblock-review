ec24d43f66acdf402d55cbf2945a0043deebb137
Dillon Dumesnil <ddumesnil@edx.org>
2017-09-26 13:38:13 -0400

    v0.66

--- review/static/js/src/review.js
+++ review/static/js/src/review.js
@@ -22,24 +22,33 @@ function ReviewXBlock(runtime, element) {
           $iframe.attr('src', $btn.data('iframe-src'));
         }
 
-        // Dynamically changes the height of the iFrame to the size of
-        // the content inside
-        if ($btn.attr('class').includes('review-button--active')) {
-          clearInterval($btn.data('intervalID'));
-        } else {
-          $btn.data('intervalID', setInterval(function() {
-            $iframe.attr('height', $iframe['0'].contentWindow.document.body.offsetHeight + 'px');
-          }, 1000));
-        }
 
-        // Possible way to check if the iFrame loaded got a 404
-        if ($iframe['0'].innerHTML) {
-          var title = $iframe['0'].contentDocument.title.toLowerCase();
-          if (title.indexOf("page not found")>=0 || title.indexOf("404")>=0) {
-            // I would like to change the HTML to display some other message. Not sure if possible
-            // $iframe.attr('src', '');
+        // Prevent issues with cross-origin frames when in studio
+        // (iFrames are hosted on courses.edx.org)
+        if (window.location.hostname.indexOf("studio") == -1) {
+          // Dynamically changes the height of the iFrame to the size of
+          // the content inside
+          if ($btn.attr('class').includes('review-button--active')) {
+            clearInterval($btn.data('intervalID'));
+          } else {
+            $btn.data('intervalID', setInterval(function() {
+              $iframe.attr('height', $iframe['0'].contentWindow.document.body.offsetHeight + 'px');
+            }, 1000));
           }
-        } else {
+
+          // Possible way to check if the iFrame loaded got a 404
+          // This shouldn't happen, but is a possibility if course teams change
+          // problems in their course before I have time to update the review course
+          if ($iframe['0'].innerHTML) {
+            var title = $iframe['0'].contentDocument.title.toLowerCase();
+            if (title.indexOf("page not found")>=0 || title.indexOf("404")>=0) {
+              // I would like to change the HTML to display some other message. Not sure if possible
+              // I think an issue here is that it redirects the actual page.
+              // Not sure how to handle that
+              // $iframe.attr('src', '');
+            }
+          } else {
+          }
         }
 
         // Toggle active state (+/-)
