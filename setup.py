43cd58cd8078da4a259ba04a99f40d190276cb44
Dillon Dumesnil <ddumesnil@edx.org>
2017-11-17 15:33:22 -0500

    v1.0.0 release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).r
 
 setup(
     name='xblock-review',
-    version='0.1',
+    version='1.0.0',
     description='The Review XBlock is designed to act as a review tool for learners in their edX course.',
     long_description = long_description,
     license='AGPL v3',
