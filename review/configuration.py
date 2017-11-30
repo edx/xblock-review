19316e6f60bfd8b2c6b51ff952a7748b940403cb
Dillon Dumesnil <ddumesnil@edx.org>
2017-11-30 14:40:30 -0500

    Fixing the course number for Circuits and Electronics 3: Applications

--- review/configuration.py
+++ review/configuration.py
@@ -10,7 +10,7 @@ SHOW_PROBLEMS = set([
     # This is here for testing purposes. Do not remove
     'DillonX/DAD101x/3T2017',
     # Anant's course
-    'course-v1:MITx+6.002.3x+2T2016',
+    'course-v1:MITx+6.002.3x_1+2T2016',
 ])
 SHOW_VERTICAL = set([
 ])
@@ -27,12 +27,12 @@ REVIEW_COURSE_MAPPING = {
     # Course used for testing. DO NOT REMOVE
     'DillonX/DAD101x/3T2017': 'DillonX/DAD101x_review/3T2017',
     # Anant's course
-    'course-v1:MITx+6.002.3x+2T2016': 'MITx+6.002.3x_review+2T2016',
+    'course-v1:MITx+6.002.3x_1+2T2016': 'MITx+6.002.3x_1_review+2T2016',
 }
 ENROLLMENT_COURSE_MAPPING = {
     # Course used for testing. DO NOT REMOVE
     'DillonX/DAD101x/3T2017': 'DillonX/DAD101x_review/3T2017',
     # Anant's course
-    'course-v1:MITx+6.002.3x+2T2016': 'course-v1:MITx+6.002.3x_review+2T2016',
+    'course-v1:MITx+6.002.3x_1+2T2016': 'course-v1:MITx+6.002.3x_1_review+2T2016',
 }
 XBLOCK_VIEW_URL_TEMPLATE = settings.LMS_ROOT_URL + '/xblock/block-v1:{course_id}+type@{type}+block@{xblock_id}'
